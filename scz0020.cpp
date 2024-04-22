62deb970493bb2d8b24d4e0355b0805a5d507740
Samuel Zimel <samuel.c.zimel@gmail.com>
2024-04-22 12:35:53 -0500

    Game got cooler! (project 2)

--- scz0020.cpp
+++ scz0020.cpp
@@ -2,61 +2,26 @@
 #include <string>
 #include <cctype>
 #include <ctime>
+#include <iomanip>
 using namespace std;
 
-int main() {
-    // variable declaration
-    int playerPoints = 100;
-    int inputChoice;
-    int inputGuess;
-    int pointsAdded = 5;
-    int pointsSubtracted = 5;
-    bool isDisplayingLeft = false;
-    bool isDisplayingRight = false;
-    int displayRight = -1; 
-    int displayLeft = -1;
+//global var for matrix size
+const int SIZE = 5;
 
-    // menu options
-    const int DisplayLeft = 1;
-    const int DisplayRight = 2;
-    const int Guess = 3;
-    const int Change = 4;
-    const int Exit = 5;
-
-    // code that only needs to run once (header, name, name formatting, srand, initial boundaries, welcome message):
-    // print out the header
-    cout << "+-------------------------------------------------+" << endl;
-    cout << "|         Computer Science and Engineering        |" << endl;
-    cout << "|         CSCE 1040 - Computer Science II         |" << endl;
-    cout << "|   Samuel Zimel scz0020 samuelzimel@my.unt.edu   |" << endl;
-    cout << "+-------------------------------------------------+" << endl;
-
-    // get user's name
-    string playerName;
-    cout << "Enter your name: ";
-    getline(cin, playerName);
-
-    // check if the name is valid && syntax
-    for (int i = 0; i < playerName.length(); i++) {
-        // capitalize the first letter of the name
-        playerName[0] = toupper(playerName[0]);
-
-        // if the name is not valid, ask for a new name
-        if (!isalpha(playerName[i]) && playerName[i] != ' ') {
-            cout << "Your name can only have alphabets or spaces. Enter again: " << endl;
-            cout << "Enter your name: ";
-            getline(cin, playerName);
-            i = 0;
+// format the name
+string formatName(string playerName) {
+    bool isFirst = true;
+    // check for first letter of each word and capitalize it
+    for (int i = 0; i < playerName.length(); ++i) {
+        if (isFirst && isalpha(playerName[i])) {
+            playerName[i] = toupper(playerName[i]);
+            isFirst = false;
+        } else if (isalpha(playerName[i])) {
+            playerName[i] = tolower(playerName[i]);
         }
 
-        // capitalize the first letter of the last name
         if (playerName[i] == ' ') {
-            playerName[i + 1] = toupper(playerName[i + 1]);
-            i++;
-
-        // lowercase the rest of the name
-        } else if ((playerName[i] != ' ') && (i != 0)) {
-            playerName[i] = tolower(playerName[i]);
+            isFirst = true;
         }
     }
 
@@ -67,144 +32,273 @@ int main() {
         playerName = playerName.substr(0, playerName.length() - 7) + ", Jr.";
     }
 
-    // welcome message
-    cout << "Welcome " << playerName << endl;
-    cout << displayRight << "          " << displayLeft << endl << endl;
+    return playerName;
+}
 
-    // generate two random numbers between 150 and 250, inclusive
-    srand(time(0));
-    int left = rand() % 101 + 150;
-    int right = rand() % 101 + 250;
+// outputs the bounds matrix
+void showBounds(int displayedBounds[2]) {
+    cout << displayedBounds[0] << "       " << displayedBounds[1] << endl;
+}
 
-    // ensure bounds are correct
-    while (left >= right) {
-        left = rand() % 101 + 150;
-        right = rand() % 101 + 250;
+// gets the name from the user => send it to be formatted
+string getName() {
+    string name;
+    bool isValid;
+    // loop that checks if the name is valid
+    while(!isValid) {
+        cout << "Enter your name: ";
+        getline(cin, name);
+
+        isValid = true;
+        for (size_t i = 0; i < name.length(); ++i) {
+            char ch = name[i];
+            if (!isalpha(ch) && !isspace(ch)) {
+                cout << "Invalid input. Only letters and spaces are allowed" << endl;
+                isValid = false;
+                break;
+            }
+        }
     }
 
-    // main game loop 
-    while (playerPoints >= 0) {
-        // display menu: 
-        cout << "Menu: " << endl;
-        cout << "1. Display the left number" << endl;
-        cout << "2. Display the right number" << endl;
-        cout << "3. Guess a number in the range (inclusive)" << endl;
-        cout << "4. Change numbers" << endl;
-        cout << "5. Exit" << endl;
+    // format valid name
+    return formatName(name);
+}
 
-        // output the displays && get the user's choice
-        cout << displayLeft << "          " << displayRight << endl;
-        cout << "Your choice: ";
-        cin >> inputChoice;
-
-        switch(inputChoice) {
-            case DisplayLeft:
-                // make sure the user can only display the left number once
-                if (isDisplayingLeft) {
-                    cout << "You have already displayed the left number." << endl;
-                    break;
-
-                // make sure the user isn't displaying the right number
-                } else if (isDisplayingRight) {
-                    cout << "You have already displayed the right boundary, you cannot display both." << endl;
-                    break;
-                }
-                // setting variables
-                isDisplayingLeft = true;
-                displayLeft = left;
-                pointsAdded = 1;
-                pointsSubtracted = 10;
-
-                // output
-                cout << "You will only get 1 point for guessing correctly and lose 10 points for guessing incorrectly, now." << endl;
-                cout << displayLeft << "          " << displayRight << endl;
-                break;
-
-            case DisplayRight:
-                // make sure the user can only display the left number once
-                if (isDisplayingLeft) {
-                    cout << "You have already displayed the left boundary, you cannot display both." << endl;
-                    break;
-
-                // make sure the user isn't displaying the right number
-                } else if (isDisplayingRight) {
-                    cout << "You have already displayed the right number." << endl;
-                    break;
-                }
-                // setting variables
-                displayRight = right;
-                isDisplayingRight = true;
-                pointsAdded = 1;
-                pointsSubtracted = 10;
-
-                // output
-                cout << "You will only get 1 point for guessing correctly and lose 10 points for guessing incorrectly, now." << endl;
-                cout << displayLeft << "          " << displayRight << endl;
-                break;
-
-            case Guess:
-                // getting input from the user
-                cout << "Enter your guess: ";
-                cin >> inputGuess;
-                cout << "Your choice: " << inputGuess << endl;
-
-                // check if the guess is within the bounds
-                if((inputGuess >= left) && (inputGuess <= right)) {
-                    playerPoints += pointsAdded;
-                    cout << "You guessed correctly. You get " << pointsAdded << " points." << endl;
-
-                // if the user isn't within the bounds
-                } else {
-                    playerPoints -= pointsSubtracted;
-                    cout << "You guessed incorrectly! You lose " << pointsSubtracted << " points." << endl;
-                }
-
-                cout << "Your remaining points = " << playerPoints << endl;
-                cout << displayLeft << "          " << displayRight << endl;
-                break;
-
-            case Change:
-                // subtracting point and output
-                playerPoints -= 1;
-                cout << "Your remaining points = " << playerPoints << endl;
-                
-                // making new boundaries
-                cout << "Generating new boundaries." << endl;
-                left = rand() % 101 + 150;
-                right = rand() % 101 + 250;
-
-                // ensure bounds are correct
-                while (left >= right) {
-                    left = rand() % 101 + 150;
-                    right = rand() % 101 + 250;
-                }
-
-                // resetting variables
-                isDisplayingLeft = false;
-                isDisplayingRight = false;
-                displayRight = -1;
-                displayLeft = -1;
-                pointsAdded = 5;
-                pointsSubtracted = 5;
-
-                // output
-                cout << displayLeft << "          " << displayRight << endl;
-                break;
-
-            case Exit:
-                cout << "Bye " << playerName << "!!!" << endl;
-                return 0;
-
-            default:
-                cout << "Wrong choice. Enter again." << endl;
+// creates a matrix of -1's
+void genShowMatrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            matrix[i][j] = -1;
         }
+    }
+}
 
-        // formatting
+// displays the matrix
+void showMatrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << setw(6) << matrix[i][j];
+        }
         cout << endl;
     }
-    // if the user runs out of points
-    cout << "You are out of points." << endl;
-    cout << "Bye " << playerName << "!!!" << endl;
+}
+
+// initialize the matrix with random numbers between lower and upper bounds
+void genHideMatrix(int matrix[SIZE][SIZE], int lower, int upper) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            matrix[i][j] = lower + rand() % (upper - lower + 1);
+        }
+    }
+}
+
+// displays the left integer in the top left corner
+void setDisplayLeft(int displayedBounds[2], int lowerBound, int& pointsAdded, int& pointsLost) {
+    // check edge cases
+    if (displayedBounds[0] == lowerBound) {
+        cout << "You have already displayed the lower bounds." << endl;
+        showBounds(displayedBounds);
+        return;
+    } else if (displayedBounds[1] != -1) {
+        displayedBounds[1] = -1;
+    }
+
+    // updated values
+    pointsAdded = 1;
+    pointsLost = 10;
+    displayedBounds[0] = lowerBound;
+
+    cout << "A correct guess will only earn 1 point, and an incorrect guess will lose 10 points." << endl;
+    showBounds(displayedBounds);
+}
+
+// displays the right integer in the bottom right corner
+void setDisplayRight(int displayedBounds[2], int upperBound, int& pointsAdded, int& pointsLost) {
+    // check edge cases
+    if (displayedBounds[1] == upperBound) {
+        cout << "You have already displayed the upper bounds." << endl;
+        showBounds(displayedBounds);
+        return;
+    } else if (displayedBounds[0] != -1) {
+        displayedBounds[0] = -1;
+    }
+
+    // updated values
+    pointsAdded = 1;
+    pointsLost = 10;
+    displayedBounds[1] = upperBound;
+
+    cout << "A correct guess will only earn 1 point, and an incorrect guess will lose 10 points." << endl;
+    showBounds(displayedBounds); 
+}
+
+// eliminates the row and column of the guessed number
+void eliminate(int visible[SIZE][SIZE], int hidden[SIZE][SIZE], int row, int col) {
+    for (int i = 0; i < SIZE; i++) {
+        visible[row][i] = 0; 
+        visible[i][col] = 0; 
+        hidden[row][i] = 0; 
+        hidden[i][col] = 0;
+    }
+}
+
+// checks if all elements in the matrix are 0
+bool allZeros(const int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (matrix[i][j] != 0) {
+                return false; 
+            }
+        }
+    }
+    // if it's gotten to this point, all elements are 0
+    return true;
+}
+
+// function to guess a number in the matrix
+void guess(int hidden[SIZE][SIZE], int visible[SIZE][SIZE], int pointsAdded, int pointsLost, int& points) {
+    // print matrix
+    showMatrix(visible);
+
+    // get user input
+    int input;
+    cout << "Enter your guess: ";
+    cin >> input;
+
+    // check if the guess is in the matrix
+    bool matchFound = false; // using bool to address the rare edge case where the same number is in the matrix twice
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (hidden[i][j] == input) {
+                eliminate(visible, hidden, i, j);
+                points += pointsAdded;
+                matchFound = true;
+                cout << "You guessed correctly. You get " << pointsAdded << " points." << endl;
+            }
+        }
+    }
+
+    if (!matchFound) {
+        cout << "No match found. You lose " << pointsLost <<  " points." << endl;
+        points -= pointsLost;
+    }
+    cout << "Your remaining points: " << points << endl;
+}
+
+
+void initialize(int hidden[SIZE][SIZE], int visible[SIZE][SIZE], int& lowerBound, int& upperBound, int displayedBounds[2]) {
+    // use time so it's truly random
+    srand(time(0));
+
+    // Generate bounds
+    lowerBound = 150 + rand() % 100; 
+    upperBound = 250 + rand() % 100;
+
+    // Initialize matrices
+    genHideMatrix(hidden, lowerBound, upperBound);
+    genShowMatrix(visible);
+
+    // reset the displayed bounds
+    displayedBounds[0] = -1;
+    displayedBounds[1] = -1;
+}
+
+int main() {
+    // Variable declarations
+    int points = 100;
+    int pointsAdded = 5;
+    int pointsLost = 5;
+    int lowerBound, upperBound;
+    int displayedBounds[2] = {-1, -1};
+    int hidden[SIZE][SIZE];
+    int visible[SIZE][SIZE];
+    int choice;
+    bool gameOver = false;
+    // menu options
+    const int DisplayLeft = 1;
+    const int DisplayRight = 2;
+    const int Guess = 3;
+    const int Reset = 4;
+    const int Exit = 5;
+
+    // intro
+    cout << "+-------------------------------------------------+" << endl;
+    cout << "|         Computer Science and Engineering        |" << endl;
+    cout << "|         CSCE 1040 - Computer Science II         |" << endl;
+    cout << "|   Samuel Zimel scz0020 samuelzimel@my.unt.edu   |" << endl;
+    cout << "+-------------------------------------------------+" << endl;
+
+    // get the player's name
+    string playerName = getName();
+    cout << "Welcome to the game, " << playerName << endl;
+
+    initialize(hidden, visible, lowerBound, upperBound, displayedBounds);
+
+    // game loop
+    while (!gameOver && points > 0) {
+        cout << "\n1. Display Left Number\n";
+        cout << "2. Display Right Number\n";
+        cout << "3. Guess a number in between\n";
+        cout << "4. Reset the game\n";
+        cout << "5. Exit\n";
+        cout << "What do you want to do? ";
+        cin >> choice;
+
+        switch (choice) {
+            case DisplayLeft:
+                setDisplayLeft(displayedBounds, lowerBound, pointsAdded, pointsLost);
+                break;
+            case DisplayRight:
+                setDisplayRight(displayedBounds, upperBound, pointsAdded, pointsLost);
+                break;
+            case Guess:
+                guess(hidden, visible, pointsAdded, pointsLost, points);
+                showBounds(displayedBounds);
+
+                if (allZeros(hidden)) {
+                    cout << "Congratulations! You have cleared the matrix." << endl;
+                    cout << "Do you want to play another game (1-yes, 0-no)? ";
+                    cin >> choice;
+                    if (choice == 1) {
+                        // reset game
+                        initialize(hidden, visible, lowerBound, upperBound, displayedBounds);
+
+                        // reset points
+                        points = 100; 
+                        pointsAdded = 5;
+                        pointsLost = 5;
+                    } else {
+                        gameOver = true;
+                    }
+                }
+                showMatrix(visible);
+                break;
+            case Reset:
+                initialize(hidden, visible, lowerBound, upperBound, displayedBounds);
+
+                // Remove a point for resetting the game
+                points -= 1;
+
+                // reset the points added and lost
+                pointsAdded = 5;
+                pointsLost = 5;
+
+                cout << "Game has been reset. Your remaining points: " << points << endl;
+                break;
+            case Exit:
+                gameOver = true;
+                break;
+            default:
+                cout << "Invalid choice, please try again.\n";
+                break;
+        }
+
+        if (points <= 0) {
+            cout << "Game over! You have run out of points.\n";
+        }
+    }
+
+    cout << "Goodbye, " << playerName << "! Your final points balance is " << points << ".\n";
 
     return 0;
 }
